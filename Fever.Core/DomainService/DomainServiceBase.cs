1e9fba1c17bf337e658a5abc792cb1cc29797bdc
jaapyang <15873115257@qq.com>
2017-09-01 00:17:06 +0800

    - unit of work,DomainService,Repository 初步构思

---
+++ Fever.Core/DomainService/DomainServiceBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using Fever.Core.Entities;
+
+namespace Fever.Core.DomainService
+{
+    public class DomainServiceBase : IDomainService
+    {
+        public IQueryable<TEntity> GetRepository<TEntity>() where TEntity : IEntity
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ Fever.Core/DomainService/IDomainService.cs
@@ -0,0 +1,11 @@
+﻿using System.Linq;
+using Fever.Core.Entities;
+
+namespace Fever.Core.DomainService
+{
+    public interface IDomainService
+    {
+        IQueryable<TEntity> GetRepository<TEntity>() 
+            where TEntity : IEntity;
+    }
+}
